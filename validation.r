66a37252a761939878cff9ae398156c78c4914e3
Weissman <Jonathan.Weissman@wildlife.ca.gov>
2019-06-28 16:51:19 -0700

    wrapped some code in functions and moved to import, analyze

--- validation.r
+++ validation.r
@@ -4,67 +4,37 @@ library(sf)
 library(lubridate)
 library(stars)
 
+source("import.r")
+source("analyze.r")
 
-## Data Import
+# Vector data
 
-# Vector Data:
+# read ram location data:
 ram_data_filename = "./shapefiles/AllRams2004_2016.shp"
-ram_data = st_read(ram_data_filename)
+ram_data = read_snbs_shapefile(ram_data_filename)
+project_crs = st_crs(ram_data)
 
-ram_data = ram_data %>% mutate(datetime = 
-           as.POSIXct(paste(as.character(Date), Time), 
-                      format ="%Y%m%d %H:%M:%S")) %>%
-  select(datetime, 
-         AnimalID,
-         UTM_E,
-         UTM_N,
-         Method,
-         DOP,
-         SV,
-         Fix,
-         CSNoDate,
-         HU,
-         RU,
-         Sex, 
-         geometry
-         )
-
-
+# read herd unit polygons:
 herd_units_filename = "./shapefiles/All_SNBS_HerdUnits.shp"
-herd_units = st_read(herd_units_filename)
-herd_units = st_transform(herd_units, 32611)
+herd_units = herd_units_filename %>% 
+  st_read %>%
+  st_transform(crs = project_crs)
 
+# read core homerange polygons:
 core_range_filename = "./shapefiles/Kernels_Merged_Dissolved.shp"
-core_range = st_read(core_range_filename)
+core_range = core_range_filename %>%
+  st_read %>%
+  st_transform(crs = project_crs)
 
-# Raster Data:
+# Raster Data
 
-# risk_tif = "./rasters/FINAL_CD_Negate_pRasout_20190613_ln_Clip60km_Rescale1_100_Invert.tif"
-# risk = read_stars(risk_tif, proxy = TRUE)
-# plot(risk)
-# rm(risk)
-
+# read rsf:
 rsf_tif = "./rasters/rsfMap_20150316.tif"
 rsf = read_stars(rsf_tif, proxy = TRUE)
-plot(rsf)
-str(rsf)
 
-
-
-## ggplot() + geom_stars(data = risk)
-  
-
-## Filters
-
-# label excursions outside of homerange
-# how to make this into function using map???
-d = ram_data %>%
-  mutate(in_core = t(st_contains(y = ., x = core_range, sparse = FALSE))) %>%
-  group_by(AnimalID) %>%
-  mutate(core_exit = lag(in_core) - in_core) %>%
-  mutate(core_exit = replace_na(core_exit, 0)) %>%
-  mutate(exit_event = cumsum( pmax(core_exit,0))) %>%
-  ungroup()
+# find excursions outside homerange:
+ram_data = ram_data %>% 
+  label_excursions(polygon = core_range)
 
 
 s20_data = d %>% filter(AnimalID == "S20")
