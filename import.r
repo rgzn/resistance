650f386c12f51a17e6c1481435110a007d94c980
Weissman <Jonathan.Weissman@wildlife.ca.gov>
2019-06-28 16:50:03 -0700

    functions related to importing data

---
+++ import.r
@@ -0,0 +1,39 @@
+library(tidyverse)
+library(sf)
+library(lubridate)
+library(stars)
+
+# Vdt_from_date_time_str
+# make datetime object from date string and time string
+dt_from_date_time_str = function (datestr, 
+                                  timestr, 
+                                  date_format = "%Y%m%d", 
+                                  time_format = "%H:%M:%S"
+) {
+  dt_str = paste(datestr, timestr)
+  dt_format = paste(date_format, time_format)
+  dt = as.POSIXct(dt_str, format = dt_format)
+  return(dt)
+}
+
+# read_snbs_shapefile
+# read a snbs formatted position shapefile
+read_snbs_shapefile = function(path) {
+  snbs_data = sf::st_read(path)
+  snbs_data %>%
+    mutate(datetime = dt_from_date_time_str(Date, Time)) %>%
+    select(datetime,
+           AnimalID,
+           UTM_E,
+           UTM_N,
+           Method,
+           DOP,
+           SV,
+           Fix,
+           CSNoDate,
+           HU,
+           RU,
+           Sex,
+           geometry
+    )
+}
--- validation.r
+++ validation.r
@@ -1,10 +1,13 @@
 library(tidyverse)
 library(sf)
-library(amt)
+# library(amt)
 library(lubridate)
+library(stars)
 
 
 ## Data Import
+
+# Vector Data:
 ram_data_filename = "./shapefiles/AllRams2004_2016.shp"
 ram_data = st_read(ram_data_filename)
 
@@ -34,21 +37,27 @@ herd_units = st_transform(herd_units, 32611)
 core_range_filename = "./shapefiles/Kernels_Merged_Dissolved.shp"
 core_range = st_read(core_range_filename)
 
-test_polygon_file = "./shapefiles/wheeler_polygon.json"
-st_layers(test_polygon_file)
-test_polygon = st_read(test_polygon_file)
-test_polygon = st_transform(test_polygon, 32611)
+# Raster Data:
+
+# risk_tif = "./rasters/FINAL_CD_Negate_pRasout_20190613_ln_Clip60km_Rescale1_100_Invert.tif"
+# risk = read_stars(risk_tif, proxy = TRUE)
+# plot(risk)
+# rm(risk)
+
+rsf_tif = "./rasters/rsfMap_20150316.tif"
+rsf = read_stars(rsf_tif, proxy = TRUE)
+plot(rsf)
+str(rsf)
+
+
+
+## ggplot() + geom_stars(data = risk)
+  
 
 ## Filters
 
-ram_data %>% 
-  filter(!st_contains(y = ., x = core_range, sparse = FALSE))
-
-ram_data %>% 
-  mutate(in_core = st_contains(y = geometry, x = core_range, sparse = FALSE))
-
-
 # label excursions outside of homerange
+# how to make this into function using map???
 d = ram_data %>%
   mutate(in_core = t(st_contains(y = ., x = core_range, sparse = FALSE))) %>%
   group_by(AnimalID) %>%
@@ -57,6 +66,8 @@ d = ram_data %>%
   mutate(exit_event = cumsum( pmax(core_exit,0))) %>%
   ungroup()
 
+
+s20_data = d %>% filter(AnimalID == "S20")
          
   
 .# only data > certain lat
@@ -66,24 +77,16 @@ north_data = ram_data %>% arrange(desc(UTM_N)) %>% slice(1)
   ## Plots
 theme_set(theme_minimal())
 
-ggplot() + 
-  geom_sf(data = test_polygon, alpha = 0.5) +
-  geom_sf(data = wh_data, 
-          aes(color = AnimalID))
-
-ggplot() + 
-  geom_sf(data = test_polygon, alpha = 0.5, fill = "green") +
-  geom_sf(data = my_point, size = 10)
-
-st_contains(x = my_point, y = test_polygon, sparse = FALSE)
-st_contains(x = test_polygon, y = my_point, sparse = FALSE)
 
 
 ggplot() + 
   geom_sf(data = core_range, alpha = 0.2) +
   geom_sf(data = s20_data %>% filter(!in_core), aes(color = as.factor(exit_event))) + 
-  coord_sf(xlim = c(292000, 308000), ylim = c(4200000, 4230000))
+  guides(color=FALSE) + 
+  coord_sf(xlim = c(293000, 309000), ylim = c(4200000, 4230000))
 
 ggplot(data = herd_units) + 
   geom_sf(aes(fill = GCU))
 
+ggplot() + 
+  geom_stars(data = risk)
