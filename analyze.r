16cd44094f110d4341f0c269b497afcb641796e2
Weissman <Jonathan.Weissman@wildlife.ca.gov>
2019-07-01 17:39:58 -0700

    added st_sample_by_sf. removed core_exit column

--- analyze.r
+++ analyze.r
@@ -10,5 +10,16 @@ label_excursions = function(points, polygon) {
     mutate(core_exit = lag(in_core) - in_core) %>%
     mutate(core_exit = replace_na(core_exit, 0)) %>%
     mutate(exit_event = cumsum( pmax(core_exit,0))) %>%
-    ungroup()
+    ungroup() %>%
+    select(-core_exit)
+}
+
+
+st_sample_by_sf = function(stars_object, sf_object) {
+  stars_object %>% 
+    aggregate(sf_object,
+              function(x) x[1], 
+              as_points = FALSE) %>% 
+    st_as_sf %>% 
+    st_intersection(sf_object)
 }
