4f69d99be269a089513c025c9b9c11c9a4f24d80
German <David.German@wildlife.ca.gov>
2020-01-18 20:55:14 -0800

    comparison of layers based on points and pseudopoints at same range

--- validation_farpoints.r
+++ validation_farpoints.r
@@ -30,16 +30,20 @@ cd_files = list.files(path = cd_dir, pattern = "*tif$", full.names = TRUE)
 cd_stars = read_rasters(path = cd_dir)
 if ( st_crs(cd_stars) != project_crs) st_transform(cd_stars, project_crs)
 
+
+# Invert rasters to make sure they are in terms of cost:
+cd_stars  = 100 - cd_stars
+
 # Correct for inconsistent cost distance rasters:
 # this section may be removed when cost distance are all in terms of cost 
 # s.t. cost increases with distance
-cd_stars <- cd_stars %>% 
-  mutate(FINALCD_Constant1.tif = 100 - FINALCD_Constant1.tif,
-         FINALCD_NegExp2.tif = 100 - FINALCD_NegExp2.tif,
-         FINALCD_NegExp4.tif = 100 - FINALCD_NegExp4.tif)
+# cd_stars <- cd_stars %>% 
+#   mutate(FINALCD_Constant1.tif = 100 - FINALCD_Constant1.tif,
+#          FINALCD_NegExp2.tif = 100 - FINALCD_NegExp2.tif,
+#          FINALCD_NegExp4.tif = 100 - FINALCD_NegExp4.tif)
 
 # create pseudopoints for each point:
-MIN_DISTANCE = units::as_units(8000, 'm') #no points below this distance are included
+MIN_DISTANCE = units::as_units(2000, 'm') #no points below this distance are included
 ram_data %>%
   tibble::rowid_to_column("id") %>%                                             # give unique id to each point
   mutate(dist_from_core =  st_distance(x = geometry, y = core_range)) %>%       # calculate distance from core
@@ -48,7 +52,7 @@ ram_data %>%
   mutate(geometry.buffer = st_buffer(geometry.core, dist = dist_from_core)) %>% # create buffer polygons
   mutate(geometry.buffer = st_cast(geometry.buffer, "MULTILINESTRING")) %>%     # convert buffers to linestrings
   mutate(buffer.circumference = st_length(geometry.buffer)) %>%                 # find circumference of buffers
-  mutate(n_samples = 50) %>%                                                    # choose number of fake points
+  mutate(n_samples = 100) %>%                                                    # choose number of fake points
   group_by(id) %>%                                                              # generate fakepoint geometries
   mutate(geometry.pseudopoints = st_union(st_sample(geometry.buffer, n_samples, type = "regular"))) %>% 
   ungroup() ->
@@ -84,7 +88,7 @@ all_data <- raster_extract_layers(cd_stars, all_data)
 all_data <- all_data %>%
   gather(key = "cost_layer",
          value = "cost",
-         contains("CD"))
+         contains("tif"))
 
 # Calculate percentile rank for each cohort of points with a shared
 #   real point and cost layer
@@ -92,16 +96,68 @@ all_data <- all_data %>%
   group_by(id, cost_layer) %>% 
   mutate(percrank = ntile(cost, 100))
 
+#################### PLOTS AND METRICS ##########################################
 
-ggplot(data = all_data %>% filter(real == TRUE, cost_layer %in% c("CD_NegLog.tif","FINALCD_NegExp4.tif") )  )+
-  geom_point(aes(x = dist_from_core, y = percrank, color = cost_layer), size = 3.0, alpha = 0.4)
+ggplot(data = all_data %>% filter(real == TRUE, cost_layer %in% c("ln.tif", 
+                                                                  "Exp8.tif", 
+                                                                  "Exp16.tif")))+
+  geom_point(aes(x = dist_from_core, y = percrank, color = cost_layer), size = 3.0, alpha = 0.1) +
+  ggtitle("Rank of ram locations costs vs. equidistant pseudopoints") +
+  xlab("Distance from core homerange [m]") + 
+  ylab("Actual location percentile cost (vs equidistant pseudopoints)")
+
+all_data %>% filter(real == TRUE, cost_layer %in% c("ln.tif", 
+                                                    "Exp4.tif", 
+                                                    "Exp16.tif")) 
+
+all_data %>% 
+  filter(real == TRUE) %>% 
+  group_by(cost_layer) %>% 
+  summarize(mean_rank = mean(percrank)) %>% 
+  arrange(mean_rank)
+
+all_data %>% 
+  as_tibble %>% 
+  filter(real == TRUE) %>% 
+  group_by(cost_layer) %>% 
+  summarize(mean_rank = mean(percrank), dist_weighted_rank = mean(percrank*dist_from_core)) %>% 
+  arrange(dist_weighted_rank)
+
+all_data %>% 
+  group_by(cost_layer, real) %>% 
+  summarize(cost_sum = sum(cost, na.rm = T)) %>% 
+  group_by(cost_layer) %>% 
+  summarize(cost_ratio = sum(cost_sum*real)/sum(cost_sum*(!real))) %>% 
+  arrange(cost_ratio)
+
+fitted_data <- all_data %>% 
+  filter(real == TRUE) %>% 
+  group_by(cost_layer) %>% 
+  do(fit = nls(percrank ~ a * dist_from_core + b, data = ., start = list(a = 0.5, b = 10))) %>% 
+  broom::augment(fit)
+
+qplot(dist_from_core, percrank, data = fitted_data, geom = 'point', colour = cost_layer) +
+  geom_line(aes(y=.fitted))
 
 
-ggplot(data = all_data %>% filter(cost_layer == "FINALCD_Constant1.tif")) +
-  geom_point(aes(x = dist_from_core, y = cost, color = real), size = 3.0, alpha = 0.4)
 
-ggplot(data = all_data %>% filter(cost_layer == "FINALCD_NegExp4.tif")) +
-  geom_point(aes(x = dist_from_core, y = cost, color = real), size = 3.0, alpha = 0.4)
+ggplot(data = all_data %>% filter(cost_layer == "Exp8.tif")) +
+  geom_point(aes(x = dist_from_core, y = cost, color = real, alpha = (0.1 + 0.9*real)))
 
-ggplot(data = all_data %>% filter(cost_layer == "CD_NegLog.tif")) +
-  geom_point(aes(x = dist_from_core, y = cost, color = real), size = 3.0, alpha = 0.4)
+ggplot(data = all_data %>% filter(cost_layer == "Exp16.tif")) +
+  geom_point(aes(x = dist_from_core, y = cost, color = real, alpha = (0.1 + 0.9*real)))
+
+ggplot(data = all_data %>% arrange(real)) +
+  facet_wrap( ~ cost_layer, ncol = 3) + 
+  geom_point(aes(x = dist_from_core, 
+                 y = cost, 
+                 color = real, 
+                 alpha = (0.05 + 0.9*real),
+                 size = 0.1 + .06*real))
+
+
+# ggplot(data = all_data %>% filter(cost_layer == "FINALCD_NegExp4.tif")) +
+#   geom_point(aes(x = dist_from_core, y = cost, color = real), size = 3.0, alpha = 0.4)
+# 
+# ggplot(data = all_data %>% filter(cost_layer == "CD_NegLog.tif")) +
+#   geom_point(aes(x = dist_from_core, y = cost, color = real), size = 3.0, alpha = 0.4)
